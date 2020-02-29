95d75dc28d8fa2856d2de41f9a8daefd6911c87b
cajgomez <Jgomez@cambridgeassociates.com>
2020-02-29 10:14:51 -0600

    Manage flow with SA with order.

--- src/main/java/com/gomezrondon/springintegration/BasicIntegrationConfig.java
+++ src/main/java/com/gomezrondon/springintegration/BasicIntegrationConfig.java
@@ -2,13 +2,17 @@ package com.gomezrondon.springintegration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.integration.config.EnableIntegration;
 import org.springframework.integration.dispatcher.LoadBalancingStrategy;
 import org.springframework.integration.dispatcher.RoundRobinLoadBalancingStrategy;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.IntegrationFlows;
-
+import org.springframework.integration.handler.MethodInvokingMessageHandler;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
 
 
 @Configuration
@@ -27,15 +31,22 @@ public class BasicIntegrationConfig{
     @Bean
     public IntegrationFlow flowHandler1() { // punto de entrada
         return IntegrationFlows.from(INPUT_CHANNEL)
-                .handle(new UppercasePrintService(),"print") //Service Activator
+                .handle(testingHandle(new UppercasePrintService(),"print",5)) //Service Activator
                 .get();
     }
 
     @Bean
     public IntegrationFlow flowHandler2() { // punto de entrada
         return IntegrationFlows.from(INPUT_CHANNEL)
-                .handle(new PrintService(),"print") //Service Activator
+                .handle(testingHandle(new PrintService(),"print",1)) //Service Activator
                 .get();
+    }
+
+    public MessageHandler testingHandle(Object object,String methodName,  Integer order) {
+        MethodInvokingMessageHandler handler = new MethodInvokingMessageHandler(object,methodName);
+        handler.setOrder(order); // the priority is a lower number
+
+        return handler;
     }
 
 
