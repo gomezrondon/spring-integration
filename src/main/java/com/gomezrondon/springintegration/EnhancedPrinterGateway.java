c9f86b0791fdd13ef4e6a498e035a6ce3b642df7
cajgomez <Jgomez@cambridgeassociates.com>
2020-03-01 15:25:29 -0600

    Adding a Header for an especific channel (method).

--- src/main/java/com/gomezrondon/springintegration/EnhancedPrinterGateway.java
+++ src/main/java/com/gomezrondon/springintegration/EnhancedPrinterGateway.java
@@ -3,14 +3,14 @@ package com.gomezrondon.springintegration;
 import org.springframework.integration.annotation.Gateway;
 import org.springframework.integration.annotation.GatewayHeader;
 import org.springframework.integration.annotation.MessagingGateway;
-
+import org.springframework.messaging.handler.annotation.Header;
 
 
 @MessagingGateway(name = "myGateway",defaultHeaders = @GatewayHeader(name = "globalHeader", value = "simpleValue"))
 public interface EnhancedPrinterGateway {
 
-    @Gateway(requestChannel = "printChannel")
-    void print(Person person);
+    @Gateway(requestChannel = "printChannel", headers =  @GatewayHeader(name = "individualHeader", expression = "#args[0].firstName")) //"#arg[0].firstName"
+    void print( Person person );
 
     @Gateway(requestChannel = "uppercaseChannel")
     String uppercase(Person person);
